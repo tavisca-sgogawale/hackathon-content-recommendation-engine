6e86b062e7f5312d5725c5b3fac9cddc24561f09
Shubham Gogawale <sgogawale@tavisca.com>
2025-06-12 18:39:02 +0530

    added csv file

--- Hackathon.ContentRecommendation.Common/Models/RealHotel.cs
+++ Hackathon.ContentRecommendation.Common/Models/RealHotel.cs
@@ -12,16 +12,18 @@ namespace Hackathon.ContentRecommendation.Common.Models
         public string CountryName { get; set; } = string.Empty;
         public int CityCode { get; set; }
         public string CityName { get; set; } = string.Empty;
-        public int HotelCode { get; set; }
-        public string HotelName { get; set; } = string.Empty;
-        public string HotelRating { get; set; } = string.Empty;
+        public int Code { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Rating { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string Attractions { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public string HotelFacilities { get; set; } = string.Empty;
-        public long Map { get; set; }
+        public string Facilities { get; set; } = string.Empty;
+        public long Lat { get; set; }
+        public long Long { get; set; }
         public string PhoneNumber { get; set; } = string.Empty;
         public int PinCode { get; set; }
         public string WebsiteUrl { get; set; } = string.Empty;
+        public string ImageUrl { get; set; }
     }
 }
